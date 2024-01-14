4d995c49f239ddc6128abded6365eb9fceb2a213
kali <arizonamarco521@gmail.com>
2024-01-15 01:30:09 +0700

    weather app - finish

--- weather-app/src/api.js
+++ weather-app/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = 'fb26fa0a32c6272b5c5c3e244232528d';
+const apiKey = 'Hehe';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 const getLocationWeather = location => {
